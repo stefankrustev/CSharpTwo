1dcd472ceb8fde608532a596f744ec16e1d7ee7e
stefankrustev <stefankrustev@me.com>
2015-02-22 22:58:52 +0200

    Numeral systems
    Homework

---
+++ NumeralSystemsHomework/BinToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,16 @@
+﻿/*Problem 2. Binary to decimal
+
+Write a program to convert binary numbers to their decimal representation.*/
+using System;
+
+class BinaryToDecimal
+{
+    static void Main()
+    {
+        Console.Write("Enter binary number: ");
+        string binary = Console.ReadLine();
+        long inDec = Convert.ToInt32(binary, 2);
+        Console.WriteLine("Your binary number {0} in decimal is {1}", binary, inDec);
+    }
+}
+
---
+++ NumeralSystemsHomework/DecimalToBin/DecimalToBinary.cs
@@ -0,0 +1,31 @@
+﻿/*Problem 1. Decimal to binary
+
+Write a program to convert decimal numbers to their binary representation.*/
+using System;
+
+class DecimalToBinary
+{
+    static void Main()
+    {
+        Console.Write("Enter some number: ");
+        int number = int.Parse(Console.ReadLine());
+        string dec = "";
+
+        while (number != 0)
+        { 
+                dec += (number % 2).ToString();
+                number = number / 2;
+        }
+
+        char[] decArr = dec.ToCharArray();
+        Array.Reverse(decArr);
+        Console.Write("Binary representation: ");
+        foreach (char s in decArr)
+        {
+            Console.Write(s);
+        }
+        Console.WriteLine();
+
+    }
+}
+
